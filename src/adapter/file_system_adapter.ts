1a23a5b94aae6078482141d3b3e6e7f871de35dc
ElRaptorus <christian.werner@5minds.de>
2019-07-10 09:54:57 +0200

    :recycle: Use log parsers in the file system adapter

--- src/adapter/file_system_adapter.ts
+++ src/adapter/file_system_adapter.ts
@@ -1,20 +1,15 @@
-import {LogEntry, LogLevel} from '@process-engine/logging_api_contracts';
-
 import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
-import * as moment from 'moment';
 import * as path from 'path';
+
+import {LogEntry} from '@process-engine/logging_api_contracts';
+
+import {parseLogEntry} from './parser/parser';
 
 export function targetExists(targetPath: string): boolean {
   return fs.existsSync(targetPath);
 }
 
-/**
- * Checks if the given path exists and creates it, if it doesn't.
- *
- * @async
- * @param targetFilePath The directory to verify.
- */
 export async function ensureDirectoryExists(targetFilePath: string): Promise<void> {
 
   // eslint-disable-next-line consistent-return
@@ -23,7 +18,6 @@ export async function ensureDirectoryExists(targetFilePath: string): Promise<voi
     const parsedPath = path.parse(targetFilePath);
 
     const targetDirectoryExists = fs.existsSync(parsedPath.dir);
-
     if (targetDirectoryExists) {
       return resolve();
     }
@@ -39,13 +33,6 @@ export async function ensureDirectoryExists(targetFilePath: string): Promise<voi
   });
 }
 
-/**
- * Writes the given entry to the specificed log file.
- *
- * @async
- * @param targetFilePath The path to the file to write to.
- * @param entry          The entry to write into the file.
- */
 export async function writeToLogFile(targetFilePath: string, entry: string): Promise<void> {
 
   return new Promise<void>((resolve: Function, reject: Function): void => {
@@ -58,13 +45,6 @@ export async function writeToLogFile(targetFilePath: string, entry: string): Pro
   });
 }
 
-/**
- * Reads all files from the given directory and parses their content into
- * readable LogEntries.
- *
- * @param dirPath The path to the directory to read.
- * @returns       The parsed logs.
- */
 export function readAndParseDirectory(dirPath: string): Array<LogEntry> {
 
   const logfileNames = fs.readdirSync(dirPath);
@@ -80,13 +60,6 @@ export function readAndParseDirectory(dirPath: string): Array<LogEntry> {
   return correlationLogs;
 }
 
-/**
- * Reads a file from the given path and parses its content into a readable
- * LogEntry.
- *
- * @param   filePath The path to the file to read.
- * @returns          The parsed log.
- */
 export function readAndParseFile(filePath: string): Array<LogEntry> {
 
   const logFileContent = fs.readFileSync(filePath, 'utf-8');
@@ -98,68 +71,7 @@ export function readAndParseFile(filePath: string): Array<LogEntry> {
     return entry.length > 0;
   });
 
-  const logEntries = logEntriesFiltered.map(createLogEntryFromRawData);
+  const logEntries = logEntriesFiltered.map(parseLogEntry);
 
   return logEntries;
-}
-
-/**
- * Takes a string representing a log entry and parses its content into a usable
- * LogEntry object.
- *
- * @param   logEntryRaw The string containing the unparsed log entry.
- * @returns             The parsed LogEntry.
- */
-// tslint:disable:no-magic-numbers
-function createLogEntryFromRawData(logEntryRaw: string): LogEntry {
-
-  const logEntryRawParts = logEntryRaw.split(';');
-
-  const isFlowNodeInstanceLog = logEntryRawParts[0] === 'FlowNodeInstance';
-
-  const logEntry = isFlowNodeInstanceLog
-    ? parseFlowNodeInstanceLog(logEntryRawParts)
-    : parseProcessModelLog(logEntryRawParts);
-
-  return logEntry;
-}
-
-/**
- * Creates a LogEntry for a FlowNodeInstance from the given data.
- *
- * @param   rawData The data to parse into a LogEntry.
- * @returns         The parsed LogEntry.
- */
-function parseFlowNodeInstanceLog(rawData: Array<string>): LogEntry {
-
-  const logEntry = new LogEntry();
-  logEntry.timeStamp = moment(rawData[1]).toDate();
-  logEntry.correlationId = rawData[2];
-  logEntry.processModelId = rawData[3];
-  logEntry.processInstanceId = rawData[4];
-  logEntry.flowNodeInstanceId = rawData[5];
-  logEntry.flowNodeId = rawData[6];
-  logEntry.logLevel = LogLevel[rawData[7]];
-  logEntry.message = rawData[8];
-
-  return logEntry;
-}
-
-/**
- * Creates a LogEntry for a ProcessModel from the given data.
- *
- * @param   rawData The data to parse into a LogEntry.
- * @returns         The parsed LogEntry.
- */
-function parseProcessModelLog(rawData: Array<string>): LogEntry {
-
-  const logEntry = new LogEntry();
-  logEntry.timeStamp = moment(rawData[1]).toDate();
-  logEntry.correlationId = rawData[2];
-  logEntry.processModelId = rawData[3];
-  logEntry.processInstanceId = rawData[4];
-  logEntry.logLevel = LogLevel[rawData[7]];
-  logEntry.message = rawData[8];
-
-  return logEntry;
 }
